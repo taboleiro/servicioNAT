18fc5cb1d0e62bfac28a4ae07853c9ab7b7af712
taboleiro <pablogaiteiro96@gmail.com>
2019-05-07 16:40:56 +0200

    Trying to modify the packets in the interfaces (in, out)

--- src/main/java/SampleTable.java
+++ src/main/java/SampleTable.java
@@ -1,5 +1,18 @@
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Random;
+
 import org.pcap4j.packet.EthernetPacket;
+import org.pcap4j.packet.IpV4Packet;
 import org.pcap4j.packet.Packet;
+import org.pcap4j.packet.TcpPacket;
+import org.pcap4j.packet.UdpPacket;
+import org.pcap4j.packet.namednumber.EtherType;
+import org.pcap4j.packet.namednumber.IpNumber;
+import org.pcap4j.packet.namednumber.TcpPort;
+import org.pcap4j.packet.namednumber.UdpPort;
 
 import gal.uvigo.det.ro1819.nat.AddrSet;
 import gal.uvigo.det.ro1819.nat.NATTable;
@@ -38,19 +51,164 @@ import gal.uvigo.det.ro1819.nat.PacketTransmission;
 */
 class SampleTable implements NATTable {
     private AddrSet addrSet;
-    
+    private String port = "5000"; 
+    Random rand = new Random();
+    PacketTransmission pTrans;
+    /* 
+     * NatTable 
+     * key: IPin_PORTin_protocol
+     * Data: arrayList: [protocol, IP_output, Port_output, IP_input, Port_input] 
+     */
+    private LinkedHashMap<String, ArrayList<String>> natTable= new LinkedHashMap<String, ArrayList<String>>();
+    private LinkedHashMap<String, String> usedPorts = new LinkedHashMap<String, String> ();
     public SampleTable(AddrSet addrSet) {
     	this.addrSet = addrSet;
     }
    
     @Override
-    public synchronized PacketTransmission getOutputPacket(Packet packet, Interface iface) {
+    public synchronized PacketTransmission getOutputPacket(Packet packet, Interface iface){
+    	ArrayList<String> tableInput = new ArrayList<String>(5);
+    	String key = new String();
+    	String outPort = "";
+    	UdpPacket udpPacket = null;
+    	TcpPacket tcpPacket = null;
+    	Inet4Address srcAddr, dstAddr = null; 
+    	Short srcPort, dstPort = 0;
+    	IpV4Packet ipv4P;
         System.err.println("Thread xestionando a interface " + 
 			   ((iface == RoNAT.Interface.OUTSIDE) ? "externa" : "interna"));
         System.err.println(packet);
-        EthernetPacket ethP = packet.get(EthernetPacket.class);
         
-        return null;
+        //"inside : outside" case
+        
+        /*
+         * IMPORTANTE: Ahora mismo no tengo en cuenta los puertos que se vienen
+         * con la tabla de reenvío inicial. Considero que no existe una tabla 
+         * inicial y por lo tanto el servivio NAT empieza de cero. 
+         * Cualquier paquete que venga de la interface outside inicialmente, 
+         * será descartado.
+         */
+	    EthernetPacket ethP = packet.get(EthernetPacket.class);
+        if (iface == RoNAT.Interface.INSIDE) {
+        	// Obtención de los datos para tabla de reenvío
+	        if (ethP.getHeader().getType() == EtherType.IPV4) {
+	        	ipv4P = packet.get(IpV4Packet.class);
+	        	if (ipv4P.getHeader().getDstAddr().toString().split(".")[0].contains("224")) {
+	        		// this packet is filtered because it contains a multicast direction
+	        		return null;
+	        	}
+	        	//if (ipv4P.getHeader().getProtocol() == IpNumber.arp)
+	        	tableInput.set(0, ipv4P.getHeader().getProtocol().toString());
+	        	tableInput.set(1, ipv4P.getHeader().getSrcAddr().toString());
+	        	tableInput.set(3, ipv4P.getHeader().getDstAddr().toString());
+	        	key = ipv4P.getHeader().getSrcAddr().toString();
+	        	if (ipv4P.getHeader().getProtocol() == IpNumber.UDP) {
+	        		udpPacket = packet.get(UdpPacket.class);
+	        		tableInput.set(2, udpPacket.getHeader().getSrcPort().toString());
+	        		key.concat(":"+udpPacket.getHeader().getSrcPort().toString());
+	        	}else if (ipv4P.getHeader().getProtocol() == IpNumber.TCP) {
+	        		tcpPacket = packet.get(TcpPacket.class);
+	        		tableInput.set(2, tcpPacket.getHeader().getSrcPort().toString());
+	        		key.concat(":"+tcpPacket.getHeader().getSrcPort().toString());
+	        	}
+	        	
+	        	if (!natTable.containsKey(key)) {
+	        		// First use of ip_in:port_in
+		        	if (usedPorts.containsKey(key.split(":")[1])) {
+		        		port = key.split(":")[1];
+		        		while (!usedPorts.containsKey(port)){
+		        			port = Integer.toString(rand.nextInt(1500) + 5000); //generate int from 5000 to 6500
+		        		}
+		        		tableInput.set(4, port);
+		        	} else {
+		        		tableInput.set(4, tableInput.get(2));
+		        	}
+		        	natTable.put(key, tableInput);
+	        	}else {
+	        		// ip_in:port_in was used before
+	    	        if (ethP.getHeader().getType() == EtherType.IPV4) {
+	    	        	ipv4P = packet.get(IpV4Packet.class);
+	    	        	key = ipv4P.getHeader().getSrcAddr().toString();
+	    	        	// 1.- add the dstPort to the packet
+	    	        	if (ipv4P.getHeader().getProtocol() == IpNumber.UDP) {
+	    	        		udpPacket = packet.get(UdpPacket.class);
+	    	        		tableInput.set(2, udpPacket.getHeader().getSrcPort().toString());
+	    	        		key.concat(":"+udpPacket.getHeader().getSrcPort().toString());
+	    	        		UdpPacket.Builder udpB = udpPacket.getBuilder();
+	    	        		udpB.dstPort(UdpPort.getInstance(Short.parseShort(natTable.get(key).get(4))));
+	    	        	}else if (ipv4P.getHeader().getProtocol() == IpNumber.TCP) {
+	    	        		tcpPacket = packet.get(TcpPacket.class);
+	    	        		tableInput.set(2, tcpPacket.getHeader().getSrcPort().toString());
+	    	        		key.concat(":"+tcpPacket.getHeader().getSrcPort().toString());
+	    	        		TcpPacket.Builder tcpB = tcpPacket.getBuilder();
+	    	        		tcpB.dstPort(TcpPort.getInstance(Short.parseShort(natTable.get(key).get(4))));
+	    	        	}
+	    	        	
+		        		//2.- source IP changed (IP_private -> IP_public)
+		        		try {
+		        			IpV4Packet.Builder ipB = ipv4P.getBuilder();
+							ipB.srcAddr( (Inet4Address)Inet4Address.getByName(natTable.get(key).get(3)));
+							ipB.build();
+						} catch (UnknownHostException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+	    	        }
+	        	}
+	        }	        
+	        
+	        
+        }else if (iface == RoNAT.Interface.OUTSIDE) {
+        	ipv4P = ethP.get(IpV4Packet.class);
+    		IpV4Packet.Builder ipB = ipv4P.getBuilder();
+	    	if (ipv4P.getHeader().getProtocol() == IpNumber.UDP) {
+	    		udpPacket = ipv4P.get(UdpPacket.class);
+	    		tableInput.set(2, udpPacket.getHeader().getSrcPort().toString());
+	    		outPort = udpPacket.getHeader().getSrcPort().toString();
+	    	}else if (ipv4P.getHeader().getProtocol() == IpNumber.TCP) {
+	    		tcpPacket = ipv4P.get(TcpPacket.class);
+	    		tableInput.set(2, tcpPacket.getHeader().getSrcPort().toString());
+	    		outPort = tcpPacket.getHeader().getSrcPort().toString();
+	    	}    		
+    		if (usedPorts.containsKey(outPort)) {
+    			// Changing source and destination port 
+        		srcAddr = ipv4P.getHeader().getDstAddr();
+        		dstAddr = (Inet4Address)Inet4Address.getByName(usedPorts.get(outPort).split(":")[0]);
+        		srcPort = Short.parseShort(natTable.get(key).get(4));
+				dstPort = Short.parseShort(natTable.get(key).get(2));
+    			if (ipv4P.getHeader().getProtocol() == IpNumber.UDP) {
+	    			UdpPacket.Builder udpB = udpPacket.getBuilder();
+	    			udpB.srcPort(UdpPort.getInstance(srcPort));
+	    			udpB.dstPort(UdpPort.getInstance(dstPort));	
+	    			udpB.dstAddr(srcAddr).srcAddr(dstAddr);
+	    			// udpB.build();
+					ipB.srcAddr(srcAddr).dstAddr(dstAddr).getPayloadBuilder(udpB);
+	        	} else if (ipv4P.getHeader().getProtocol() == IpNumber.TCP) {
+	    			TcpPacket.Builder tcpB = tcpPacket.getBuilder();
+	    			tcpB.srcPort(TcpPort.getInstance(srcPort));
+	    			tcpB.dstPort(TcpPort.getInstance(dstPort));
+	    			tcpB.dstAddr(srcAddr).srcAddr(dstAddr);
+	    			// tcpB.build();
+					ipB.srcAddr(srcAddr).dstAddr(dstAddr).getPayloadBuilder(tcpB);
+	        	}
+	        	
+    			// Changing source and destination IPaddress
+    			try {
+					ipB.srcAddr(srcAddr).dstAddr(dstAddr).getPayloadBuilder();
+					ipB.dstAddr();
+					ipB.build();
+				} catch (UnknownHostException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+    		} else {
+    			return null;
+    		}
+        	
+        }
+	    //packet construction and transmission 
+    	pTrans = new PacketTransmission(packet, iface);
+        return pTrans;
     }
 
 }
--- src/main/java/gal/uvigo/det/ro1819/nat/RoNAT.java
+++ src/main/java/gal/uvigo/det/ro1819/nat/RoNAT.java
@@ -33,13 +33,13 @@ import org.pcap4j.util.MacAddress;
 
 public class RoNAT {
     public enum Interface {
-	INSIDE,
-	OUTSIDE
+		INSIDE,
+		OUTSIDE
     };
 
-    private static final String DEFAULTNETWORK = "10.99.100.0/24";
-    private static final String DEFAULTIIFACE = "tap0";
-    private static final String DEFAULTOIFACE = "br0";
+    private static final String DEFAULTNETWORK = "10.99.100.0/24"; //MÁSCARA DE RED POR DEFECTO DE LOS EQUIPOS
+    private static final String DEFAULTIIFACE = "tap0"; //INPUT INTERFACE
+    private static final String DEFAULTOIFACE = "br0"; //OUTPUT INTERFACE
     private static final int SNAPSHOTLENGTH = 65536; // bytes   
     private static final int READTIMEOUT = 50; // mseg                   
     
@@ -49,135 +49,156 @@ public class RoNAT {
     private PcapNetworkInterface iiDev;
     private PcapNetworkInterface oiDev;
     private AddrSet addresses;
-
+    
+    /**
+     * This function create a default RoNAT router
+     * @param hostRouterMacAddr
+     * @return
+     * @throws PcapNativeException
+     */
     public static RoNAT create(MacAddress hostRouterMacAddr) throws PcapNativeException {
-	return create(hostRouterMacAddr, DEFAULTNETWORK, DEFAULTIIFACE, DEFAULTOIFACE);
+    	return create(hostRouterMacAddr, DEFAULTNETWORK, DEFAULTIIFACE, DEFAULTOIFACE);
     }
     
+    /**
+     * Create a RoNAT router with different values than the default ones
+     * @param hostRouterMacAddr
+     * @param localNetwork
+     * @param iiface
+     * @param oiface
+     * @return
+     * @throws PcapNativeException
+     */
     public static RoNAT create(MacAddress hostRouterMacAddr, String localNetwork, String iiface, String oiface)
 	throws PcapNativeException {
-	if (self == null) 	   
-	    self = new RoNAT(hostRouterMacAddr, localNetwork, iiface, oiface);
-
-	return self;	
+		if (self == null) 	   
+		    self = new RoNAT(hostRouterMacAddr, localNetwork, iiface, oiface);
+	
+		return self;	
     }
 
-
+    /**
+     * @param hostRouterMacAddr 
+     * @param localNetwork 
+     * @param iiface input Network device name
+     * @param oiface output Network device name
+     * @return O paquete modificado ou null se se quere filtrar o paquete.
+     */
     private RoNAT(MacAddress hostRouterMacAddr, String localNetwork, String iiface, String oiface)
 	throws PcapNativeException {
-	this.localNetwork = localNetwork;
-
-	iiDev = getNetworkDevice(iiface);		
-	if (iiDev == null) {
-	    System.err.println("Error abrindo interface " + iiface);
-	    System.exit(1);
-	}
+		this.localNetwork = localNetwork;
 	
-	oiDev = getNetworkDevice(oiface);
-	if (oiDev == null) {
-	    System.err.println("Error abrindo interface " + oiface);
-	    System.exit(1);
-	}
-
-	var laddr1 = iiDev.getAddresses();
-	var laddr2 = oiDev.getAddresses();
-	var lmac1 = iiDev.getLinkLayerAddresses();
-	var lmac2 = oiDev.getLinkLayerAddresses();
-	addresses = new AddrSet((Inet4Address) laddr1.get(0).getAddress(),
-					 (Inet4Address) laddr2.get(1).getAddress(),
-					 (MacAddress) lmac1.get(0),
-					 (MacAddress) lmac2.get(0),
-					 (MacAddress) hostRouterMacAddr);
-    }
-
-    
+		iiDev = getNetworkDevice(iiface);		
+		if (iiDev == null) {
+		    System.err.println("Error abrindo interface " + iiface);
+		    System.exit(1);
+		}
+		
+		oiDev = getNetworkDevice(oiface);
+		if (oiDev == null) {
+		    System.err.println("Error abrindo interface " + oiface);
+		    System.exit(1);
+		}
+	
+		var laddr1 = iiDev.getAddresses();
+		var laddr2 = oiDev.getAddresses();
+		var lmac1 = iiDev.getLinkLayerAddresses();
+		var lmac2 = oiDev.getLinkLayerAddresses();
+		addresses = new AddrSet((Inet4Address) laddr1.get(0).getAddress(),
+						 (Inet4Address) laddr2.get(1).getAddress(),
+						 (MacAddress) lmac1.get(0),
+						 (MacAddress) lmac2.get(0),
+						 (MacAddress) hostRouterMacAddr);
+	    }
+	
+	    
     private PcapNetworkInterface getNetworkDevice(String devname) throws PcapNativeException {
-	var device = Pcaps.findAllDevs().stream().filter(dev -> devname.equals(dev.getName())).findAny();
-
-	if (device.isPresent()) {
-	    return device.get();		
-	}
-
-	return null;       
-    }
-
-    public AddrSet getAddresses() {
-	return addresses;
+		var device = Pcaps.findAllDevs().stream().filter(dev -> devname.equals(dev.getName())).findAny();
+	
+		if (device.isPresent()) {
+		    return device.get();		
+		}
+	
+		return null;       
+	    }
+	
+	    public AddrSet getAddresses() {
+		return addresses;
     }
     
     public void execute(NATTable table) throws PcapNativeException, NotOpenException  {	
-	final PcapHandle handleIn, sendHandleIn, handleOut, sendHandleOut;
+		final PcapHandle handleIn, sendHandleIn, handleOut, sendHandleOut;
+		
+		// Apertura dos dispositivos, obtención dos "handlers" e configuración dos filtros
+		handleIn = iiDev.openLive(SNAPSHOTLENGTH, PromiscuousMode.PROMISCUOUS, READTIMEOUT);
+		
+		handleOut = oiDev.openLive(SNAPSHOTLENGTH, PromiscuousMode.PROMISCUOUS, READTIMEOUT);
+		sendHandleIn = iiDev.openLive(SNAPSHOTLENGTH, PromiscuousMode.PROMISCUOUS, READTIMEOUT);
+		sendHandleOut = oiDev.openLive(SNAPSHOTLENGTH, PromiscuousMode.PROMISCUOUS, READTIMEOUT);
 	
-	// Apertura dos dispositivos, obtención dos "handlers" e configuración dos filtros
-	handleIn = iiDev.openLive(SNAPSHOTLENGTH, PromiscuousMode.PROMISCUOUS, READTIMEOUT);
 	
-	handleOut = oiDev.openLive(SNAPSHOTLENGTH, PromiscuousMode.PROMISCUOUS, READTIMEOUT);
-	sendHandleIn = iiDev.openLive(SNAPSHOTLENGTH, PromiscuousMode.PROMISCUOUS, READTIMEOUT);
-	sendHandleOut = oiDev.openLive(SNAPSHOTLENGTH, PromiscuousMode.PROMISCUOUS, READTIMEOUT);
-
-
-	String filterIn = "((arp[6:2] = 2) and (ether dst " + Pcaps.toBpfString(addresses.getInnerMac())
-	    + ")) or ip src net " + localNetwork; 
-	handleIn.setFilter(filterIn, BpfCompileMode.OPTIMIZE);
-	
-	String dstHost = addresses.getOuterIP().getHostAddress();
-	String filterOut = "((arp[6:2] = 2) and (ether dst " + Pcaps.toBpfString(addresses.getOuterMac())
-	    + ")) or ip dst host " + dstHost; 
-	handleOut.setFilter(filterOut, BpfCompileMode.OPTIMIZE);		
-	
-	var tIn = new PacketHandler(handleIn, sendHandleIn, sendHandleOut, Interface.INSIDE, table);
-	tIn.start();
-	
-	var tOut = new PacketHandler(handleOut, sendHandleIn, sendHandleOut, Interface.OUTSIDE, table);
-	tOut.start();
+		String filterIn = "((arp[6:2] = 2) and (ether dst " + Pcaps.toBpfString(addresses.getInnerMac())
+		    + ")) or ip src net " + localNetwork; 
+		handleIn.setFilter(filterIn, BpfCompileMode.OPTIMIZE);
+		
+		String dstHost = addresses.getOuterIP().getHostAddress();
+		String filterOut = "((arp[6:2] = 2) and (ether dst " + Pcaps.toBpfString(addresses.getOuterMac())
+		    + ")) or ip dst host " + dstHost; 
+		handleOut.setFilter(filterOut, BpfCompileMode.OPTIMIZE);		
+		
+		var tIn = new PacketHandler(handleIn, sendHandleIn, sendHandleOut, Interface.INSIDE, table);
+		tIn.start();
+		
+		var tOut = new PacketHandler(handleOut, sendHandleIn, sendHandleOut, Interface.OUTSIDE, table);
+		tOut.start();
     } 
         
     private static class PacketHandler extends Thread {	
-	private PcapHandle handleRX;
-	private PcapHandle handleTxIn;
-	private PcapHandle handleTxOut;
-	private PacketListener listener;
-	private NATTable table;
-	private Interface iface;
-	
-	public PacketHandler(PcapHandle rx, PcapHandle txin, PcapHandle txout, Interface iface, NATTable NAT) {
-	    handleTxIn = txin;
-	    handleTxOut = txout;
-	    handleRX = rx;
-	    table = NAT;
-	    this.iface = iface;
-	}
-	
-	@Override
-	public void run() {
-	    listener = packet -> {	
- 		PacketTransmission pktTX = table.getOutputPacket(packet, iface);
-		Packet newP = pktTX.getPacket();
-	        Interface txIface = pktTX.getTxInterface();
-
-		try {
-		    if (newP != null) {			
-			var handle = (txIface == RoNAT.Interface.INSIDE) ? handleTxIn : handleTxOut;
-		    
-			handle.sendPacket(newP);
-		    }
-		} catch (PcapNativeException | NotOpenException e) {
-		    // TODO Auto-generated catch block
-		    e.printStackTrace();
-		    return;		  
+		private PcapHandle handleRX;
+		private PcapHandle handleTxIn;
+		private PcapHandle handleTxOut;
+		private PacketListener listener;
+		private NATTable table;
+		private Interface iface;
+		
+		public PacketHandler(PcapHandle rx, PcapHandle txin, PcapHandle txout, Interface iface, NATTable NAT) {
+		    handleTxIn = txin;
+		    handleTxOut = txout;
+		    handleRX = rx;
+		    table = NAT;
+		    this.iface = iface;
 		}
-	    };
-
-	    try {
-		handleRX.loop(-1, this.listener);
-	    } catch (PcapNativeException | InterruptedException | NotOpenException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	    } finally {		
-		handleRX.close();
-		handleTxIn.close();
-		handleTxOut.close();
-	    }
-	}
+		
+		@Override
+		public void run() {
+		    listener = packet -> {	
+		 		PacketTransmission pktTX = table.getOutputPacket(packet, iface);
+				Packet newP = pktTX.getPacket();
+			        Interface txIface = pktTX.getTxInterface();
+		
+				try {
+				    if (newP != null) {			
+					var handle = (txIface == RoNAT.Interface.INSIDE) ? handleTxIn : handleTxOut;
+				    
+					handle.sendPacket(newP);
+				    }
+				} catch (PcapNativeException | NotOpenException e) {
+				    // TODO Auto-generated catch block
+				    e.printStackTrace();
+				    return;		  
+				}
+		    };
+	
+		    try {
+		    	handleRX.loop(-1, this.listener);
+		    } catch (PcapNativeException | InterruptedException | NotOpenException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		    } finally {		
+				handleRX.close();
+				handleTxIn.close();
+				handleTxOut.close();
+		    }
+		}
     }        
 } 
